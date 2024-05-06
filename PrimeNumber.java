45908924e3c32001f929675397bfcf133136a673
suhasghatkamble <suhas27ghatkamble@gmail.com>
2024-05-06 19:20:04 +0530

    my first commit

---
+++ PrimeNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("prime no check .. please enter no.");
+        int num = sc.nextInt();
+        boolean isPrime = true;
+        for(int i =2; i<num/2;i++){
+            if(num % i == 0){
+                isPrime =false;
+                break;
+
+            }
+        } 
+
+
+        if(isPrime)
+        System.out.println(num+"is a prime no");
+        else
+        System.out.println(num+"is not a prime no");
+        sc.close();
+    }
+}
